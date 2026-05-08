3d8f13d78612b211929b80aa94467a725c6a7195
Luke Honiss <luke@solvedata.io>
2026-05-08 18:50:54 +1200

    Fix evision dep

--- tabletop/mix.exs
+++ tabletop/mix.exs
@@ -70,7 +70,8 @@ defmodule Tabletop.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.2.0"},
       {:bandit, "~> 1.5"},
-      {:eqrcode, "~> 0.2"}
+      {:eqrcode, "~> 0.2"},
+      {:evision, "~> 0.2"}
     ]
   end
 
